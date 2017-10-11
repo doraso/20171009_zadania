d871476d4e1728b0fc71a15774a3a291065fb3fd
Dorota Szwed <dorota.soroko@gmail.com>
2017-10-11 22:28:18 +0200

    Zadanie 1 - warunki niekontrolowane

---
+++ src/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String pesel;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public Person(String firstName, String lastName, int age, String pesel) {
+        if (firstName == null || lastName == null || (firstName.length()<=2) || (lastName.length()<=2)){
+            throw new NameUndefinedException();
+        }
+        if (age<1){ throw new IncorrectAgeException();
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public String toString(){
+        return firstName + " " + lastName + ", wiek: " + age + ", pesel: " + pesel;
+    }
+}
---
+++ src/PersonTest.java
@@ -0,0 +1,31 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+public class PersonTest {
+    public static void main(String[] args)  {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Podaj imię");
+        String firstName = scan.nextLine();
+        System.out.println("Podaj nazwisko");
+        String lastName = scan.nextLine();
+
+        System.out.println("Podaj wiek");
+        int age = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Podaj pesel");
+        String pesel = scan.nextLine();
+        try {
+            Person person = new Person(firstName, lastName, age, pesel);
+            System.out.println(person.toString());
+        } catch (NameUndefinedException ex) {
+            System.err.println("Wartość zbyt krótka lub null");
+        } catch (IncorrectAgeException ex) {
+            System.err.println("Podany wiek jest mniejszy od 1");
+        }
+
+
+    }
+}
